47cbd1cc3d1d7a2511fa8f17729e476936519ce6
roman89ua <roman89ua@gmail.com>
2020-08-02 22:36:57 +0300

    work with the numbers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,3 +16,4 @@ import {FormsModule} from '@angular/forms';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
