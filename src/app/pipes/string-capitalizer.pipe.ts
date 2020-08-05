c0d4143173c096a8f86aee2f5879490082c6415c
roman89ua <roman89ua@gmail.com>
2020-08-05 12:39:39 +0300

    refactored code

--- src/app/pipes/string-capitalizer.pipe.ts
+++ src/app/pipes/string-capitalizer.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class StringCapitalizerPipe implements PipeTransform {
 
   transform(value: string): string {
-    const arr = value.trim().split(' ');
+    const arr: string[] = value.trim().split(' ');
     arr.forEach((v, i) => arr[i] = v.charAt(0).toUpperCase() + v.slice(1));
     return arr.join(' ');
   }
